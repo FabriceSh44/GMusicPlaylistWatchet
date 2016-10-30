6238a66fe66c2653cc812abb74e72a2c4a3bac31
fabriceedon <fabrice.edon@gmail.com>
2016-10-29 21:41:27 -0400

    add track class

--- Playlist.py
+++ Playlist.py
@@ -5,14 +5,20 @@ class Playlist():
         pass
 
     def add_track(self, album, artist, title):
-        self.track_list.append((album, artist, title))
+        self.track_list.append(Track(album, artist, title))
 
     def __str__(self):
         return '{0} - {1} tracks'.format(self.name, len(self.track_list))
 
     def has_track(self, track_to_find):
         for track in self.track_list:
-            if track[0] == track_to_find[0] and track[1] == track_to_find[1] and track[2] == track_to_find[2]:
+            if track.album == track_to_find.album and track.artist == track_to_find.artist and track.title == track_to_find.title:
                 return True
         return False
 
+
+class Track():
+    def __init__(self, album, artist, title):
+        self.album = album
+        self.artist = artist
+        self.title = title
